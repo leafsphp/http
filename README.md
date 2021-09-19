7e230f165860cd43b8430e891ae002629b1ac744
Mychi <mickdd22@gmail.com>
2021-09-19 11:16:47 +0000

    :tada: completed project setup

---
+++ README.md
@@ -0,0 +1,27 @@
+<!-- markdownlint-disable no-inline-html -->
+<p align="center">
+  <br><br>
+  <img src="https://leaf-docs.netlify.app/images/logo.png" height="100"/>
+  <h1 align="center">Leaf HTTP Module</h1>
+  <br><br>
+</p>
+
+# Leaf PHP
+
+[![Latest Stable Version](https://poser.pugx.org/leafs/http/v/stable)](https://packagist.org/packages/leafs/http)
+[![Total Downloads](https://poser.pugx.org/leafs/http/downloads)](https://packagist.org/packages/leafs/http)
+[![License](https://poser.pugx.org/leafs/http/license)](https://packagist.org/packages/leafs/http)
+
+Leaf's core http functionality packaged as a serve-yourself module. Although seperated from Leaf core, it is still part of the default initial installation and doesn't need to be installed manually (unless you want a particular version).
+
+## Installation
+
+You can easily install Leaf using [Composer](https://getcomposer.org/).
+
+```bash
+composer require leafs/http
+```
+
+## View Leaf's docs [here](https://leafphp.netlify.app/#/)
+
+Built with ❤ by [**Mychi Darko**](https://mychi.netlify.app)
---
+++ src/Headers.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Leaf\Http;
+
+/**
+ * HTTP Headers
+ * ---------------------
+ * Response header management made simple with Leaf
+ *
+ * @author Michael Darko
+ * @since 2.0.0
+ */
+class Headers
+{
+    protected static $http_code;
+
+    /**
+     * Get or Set an HTTP code for response
+     *
+     * @param int|null $http_code The current response code.
+     */
+    public static function status($http_code = null)
+    {
+        if (!$http_code) return self::$http_code;
+        self::$http_code = $http_code;
+    }
+
+    /**
+     * Force an HTTP code for response using PHP's `http_response_code`
+     */
+    public static function resetStatus($http_code = null)
+    {
+        return http_response_code($http_code);
+    }
+
+    /**
+     * Get all headers passed into application
+     *
+     * @param bool $safeOutput Try to sanitize header data
+     */
+    public static function all($safeOutput = false): array
+    {
+        if ($safeOutput === false) return self::findHeaders();
+        return \Leaf\Anchor::sanitize(self::findHeaders());
+    }
+
+    /**
+     * Return a particular header passed into app
+     *
+     * @param string|array $param The header(s) to return
+     * @param bool $safeOutput Try to sanitize header data
+     *
+     * @return string|array
+     */
+    public static function get($params, $safeOutput = false)
+    {
+        if (is_string($params)) return self::all($safeOutput)[$params] ?? null;
+
+        $data = [];
+        foreach ($params as $param) {
+            $data[$param] = self::get($param, $safeOutput);
+        }
+        return $data;
+    }
+
+    /**
+     * Set a new header
+     */
+    public static function set($key, $value = "", $replace = true, $http_code = null): void
+    {
+        if (!is_array($key)) {
+            header("$key: $value", $replace, $http_code ?? self::$http_code);
+        } else {
+            foreach ($key as $header => $header_value) {
+                self::set($header, $header_value, $replace, $http_code);
+            }
+        }
+    }
+
+    public static function remove($keys)
+    {
+        if (!is_array($keys)) {
+            header_remove($keys);
+        } else {
+            foreach ($keys as $key) {
+                self::remove($key);
+            }
+        }
+    }
+
+    public static function contentPlain($code = null): void
+    {
+        self::set("Content-Type", "text/plain", true, $code ?? self::$http_code);
+    }
+
+    public static function contentHtml($code = null): void
+    {
+        self::set("Content-Type", "text/html", true, $code ?? self::$http_code);
+    }
+
+    public static function contentJSON($code = null): void
+    {
+        self::set("Content-Type", "application/json", true, $code ?? self::$http_code);
+    }
+
+    public static function accessControl($key, $value = "", $code = null)
+    {
+        if (is_string($key)) {
+            self::set("Access-Control-$key", $value, true, $code ?? self::$http_code);
+        } else {
+            foreach ($key as $header => $header_value) {
+                self::accessControl($header, $header_value, $code);
+            }
+        }
+    }
+
+    protected static function findHeaders()
+    {
+        if (function_exists("getallheaders") && \getallheaders()) return \getallheaders();
+
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if ((substr($name, 0, 5) == 'HTTP_') || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH')) {
+                $headers[str_replace([' ', 'Http'], ['-', 'HTTP'], ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
