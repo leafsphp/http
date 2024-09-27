c506a576a7dc23275a81d68993761c57fc0ced8f
Torben Buck <Rasalas@users.noreply.github.com>
2024-09-27 13:16:40 +0200

    add custom responses (fix #21) (#22)
    * add custom echo response
    
    resolves #21
    
    * add js response

--- src/Response.php
+++ src/Response.php
@@ -55,6 +55,21 @@ class Response
     }
 
     /**
+     * Output any text
+     * 
+     * @param string $data The data to output
+     * @param int $code The response status code
+     */
+    public function echo(string $data, int $code = 200)
+    {
+        $this->status = $code;
+        $this->headers['Content-Type'] ??= 'text/plain';
+        $this->content = $data;
+
+        $this->send();
+    }
+
+    /**
      * Output plain text
      *
      * @param mixed $data The data to output
@@ -79,6 +94,21 @@ class Response
     {
         $this->status = $code;
         $this->headers['Content-Type'] = 'application/xml';
+        $this->content = $data;
+
+        $this->send();
+    }
+    
+    /**
+     * Output js script
+     * 
+     * @param string $data The data to output
+     * @param int $code The response status code
+     */
+    public function js(string $data, int $code = 200)
+    {
+        $this->status = $code;
+        $this->headers['Content-Type'] = 'text/javascript';
         $this->content = $data;
 
         $this->send();
